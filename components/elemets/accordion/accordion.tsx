829f37f6e3220c0c2f7f735b9ab49ccf8bae06a6
hikari <tanimoto0709@gmail.com>
2023-05-21 07:55:35 +0900

    [WIP]アコーディオンのスタイル調整

--- components/elemets/accordion/accordion.tsx
+++ components/elemets/accordion/accordion.tsx
@@ -1,5 +1,6 @@
 // 入れ子できるようなアコーディオンは別のコンポーネントとして定義する
 import { ReactNode } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
 
 type Props = {
   summary: string;
@@ -9,8 +10,11 @@ type Props = {
 const Accordion = (props: Props): JSX.Element => {
   return (
     <details>
-      <summary>{props.summary}</summary>
-      {props.children}
+      <summary className="block flex items-center">
+        {props.summary}
+        <ChevronDownIcon className="w-5 h-5 text-gray-500 ml-2 transition duration-300" />
+      </summary>
+      <div className="transition duration-300">{props.children}</div>
     </details>
   );
 };
