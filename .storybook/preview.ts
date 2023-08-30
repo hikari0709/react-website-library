a7439ccc10e6f5966649132b2cd0d36d76192aa9
hikari <tanimoto0709@gmail.com>
2023-08-30 20:39:17 +0900

    update: storybookの設定

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,9 @@
-import type { Preview } from "@storybook/react";
+import '../styles/globals.css';
+import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
