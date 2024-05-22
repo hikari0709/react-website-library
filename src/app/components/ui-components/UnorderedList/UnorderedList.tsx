9ce8c3819b77b3f0cc3e77cc67b25b2134b538ab
hikari <tanimoto0709@gmail.com>
2024-05-23 06:44:47 +0900

    update: 型の定義を修正

--- src/app/components/ui-components/UnorderedList/UnorderedList.tsx
+++ src/app/components/ui-components/UnorderedList/UnorderedList.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import { FC, ComponentProps } from 'react';
+import { twJoin } from 'tailwind-merge';
 
-type Props = ComponentProps<'ul'>;
+type Props = Omit<ComponentProps<'ul'>, 'role'>;
 
-export const UnorderedList: FC<Props> = ({ children }) => {
+export const UnorderedList: FC<Props> = ({ children, className, ...atr }) => {
   return (
     <ul
       role="list"
-      className="list-disc flex flex-col gap-y-1"
+      className={twJoin("list-disc flex flex-col gap-y-1", className)}
+      {...atr}
     >
       {children}
     </ul>
