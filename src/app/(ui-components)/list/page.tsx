5c3dad5b31d46ad87e4c1655a35cc2eb7a896f1d
hikari <tanimoto0709@gmail.com>
2024-05-22 19:59:35 +0900

    pageに表示されるように修正

--- src/app/(ui-components)/list/page.tsx
+++ src/app/(ui-components)/list/page.tsx
@@ -1,10 +1,17 @@
 import React, { FC, PropsWithChildren, ReactNode } from 'react';
 import { UnorderedList } from '@/src/app/components/ui-components/UnorderedList';
+import { ListItem } from '@/components/ListItem';
 
 const ListView = () => {
   return (
     <section>
-      <UnorderedList />
+      <UnorderedList>
+        <ListItem>ListItem 1</ListItem>
+        <ListItem>ListItem 2</ListItem>
+        <ListItem>ListItem 3</ListItem>
+        <ListItem>ListItem 4</ListItem>
+        <ListItem>ListItem 5</ListItem>
+      </UnorderedList>
     </section>
   );
 };
