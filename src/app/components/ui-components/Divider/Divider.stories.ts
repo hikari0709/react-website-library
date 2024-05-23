eb474f6e19b78c01bc8e6eac1606a20c94b56f62
hikari <tanimoto0709@gmail.com>
2024-05-23 09:49:38 +0900

    update: Divider

--- src/app/components/ui-components/Divider/Divider.stories.ts
+++ src/app/components/ui-components/Divider/Divider.stories.ts
@@ -12,7 +12,24 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
+    horizontal: false,
     type: 'solid',
-    spacing: '1',
+    spacing: 1,
+  },
+};
+
+export const Dashed: Story = {
+  args: {
+    horizontal: false,
+    type: 'dashed',
+    spacing: 1,
+  },
+};
+
+export const Dotted: Story = {
+  args: {
+    horizontal: false,
+    type: 'dotted',
+    spacing: 1,
   },
 };
--- src/app/components/ui-components/Divider/Divider.tsx
+++ src/app/components/ui-components/Divider/Divider.tsx
@@ -1,12 +1,38 @@
 'use client';
 import { FC } from 'react';
-import { tv } from 'tailwind-variants';
+import { tv, type VariantProps } from 'tailwind-variants';
+
+const divider = tv({
+  base: 'border-gray-400',
+  variants: {
+    horizontal: {
+      true: 'border-l',
+      false: 'border-t',
+    },
+    type: {
+      solid: 'border-solid',
+      dashed: 'border-dashed',
+      dotted: 'border-dotted',
+    },
+    spacing: {
+      1: 'mt-1',
+      2: 'mt-2',
+      4: 'mt-4',
+      8: 'mt-8',
+    },
+  },
+});
+
+type DividerVariants = VariantProps<typeof divider>;
 
 type Props = {
-  type: string;
-  spacing: string;
-};
+  horizontal?: boolean;
+} & DividerVariants;
 
-export const Divider: FC<Props> = ({ type, spacing }) => {
-  return <div className="border-t border-black"></div>;
+export const Divider: FC<Props> = ({
+  horizontal = false,
+  type = 'solid',
+  spacing = 1,
+}) => {
+  return <div className={divider({ horizontal, type, spacing })}></div>;
 };
